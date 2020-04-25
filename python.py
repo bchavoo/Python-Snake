73b1cfe48f9682d15d401d1afd2771757e79f4eb
chavezbryanr <chavezbryanr@gmail.com>
2020-04-25 04:01:06 -0500

    Snake object
    Added a snake object with some possible functions it will use

--- python.py
+++ python.py
@@ -7,5 +7,18 @@
     I want to develop a small game of snake, called "Python".
     This will help me review and refamilarize myself with Python language,
     such as simple logic and object movement.
-    
+
 '''
+
+class snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
