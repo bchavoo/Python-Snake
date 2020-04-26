890d91b37d0f1ad8adae5ef7386933e5c90eb37a
chavezbryanr <chavezbryanr@gmail.com>
2020-04-26 03:50:38 -0500

    Window and grid
    The program is able to open a grid that will be used for the game

--- python.py
+++ python.py
@@ -9,6 +9,7 @@
     such as simple logic and object movement.
 
 '''
+import pygame
 
 class snake(object):
     def __init__(self, color, pos):
@@ -32,3 +33,44 @@ class cube(object):
 
     def draw(self):
         pass
+
+def drawGrid(w, rows, surface):
+    sizeBetween = w // rows
+    x = 0
+    y = 0
+    for i in range(rows):
+        x = x + sizeBetween
+        y = y + sizeBetween
+
+        #Drawing horizonal and vertical lines
+        pygame.draw.line(surface, (255,255,255), (x,0),(x,w))
+        pygame.draw.line(surface, (255,255,255), (0,y),(w,y))
+
+def redrawWindow(surface):
+    global rows, width
+    surface.fill((0,0,0))
+    drawGrid(width, rows, surface)
+    pygame.display.update()
+
+def main():
+    global width, height, rows
+    width = 700
+    height = 700
+    rows = 20
+    window = pygame.display.set_mode((width, height))
+
+    s = snake((0,255,0), (10,10))
+
+    flag = True
+
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.event.get()
+        pygame.time.delay(50)
+        clock.tick(10)
+        redrawWindow(window)
+
+
+    pass
+main()
