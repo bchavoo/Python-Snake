0231dbc1288fc6b9a2e532cf957fa43242b07225
chavezbryanr <chavezbryanr@gmail.com>
2020-04-28 04:04:34 -0500

    Standstill to movement
    I went from a standstill snake to a moving snake in the game

--- python.py
+++ python.py
@@ -12,11 +12,66 @@
 import pygame
 
 class snake(object):
+    body = []
+    turns = {}
     def __init__(self, color, pos):
-        pass
+        self.color = color
+        self.head = cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 0
 
     def move(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            #Keep updating keyboard input
+            keys = pygame.key.get_pressed()
+
+            for key in keys:
+                #Checking each arrow key explicitly allowing only 1 pressed at time
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_UP]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_DOWN]:
+                    self.dirnx = 0
+                    self.dirny = 1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+        for i,cube in enumerate(self.body):
+            p = cube.pos[:]
+            #Keeping track of turns in a list
+            if p in self.turns:
+                turn = self.turns[p]
+                cube.move(turn[0], turn[1])
+                if i == len(self.body)-1:
+                    self.turns.pop(p)
+            else:
+                #These 4 bring the snake across the screen at walls
+                #We can change it later to die if it hits the wall
+                if cube.dirnx == -1 and cube.pos[0] <= 0:
+                    cube.pos = (cube.rows-1, cube.pos[1])
+                elif cube.dirnx == 1 and cube.pos[0] >= cube.rows-1:
+                    cube.pos = (0, cube.pos[1])
+                elif cube.dirny == 1 and cube.pos[1] >= cube.rows-1:
+                    cube.pos = (cube.pos[0], 0)
+                elif cube.dirny == -1 and cube.pos[1] <= 0:
+                    cube.pos = (cube.pos[0], cube.rows-1)
+                else:
+                    cube.move(cube.dirnx,cube.dirny)
 
     def reset(self, pos):
         pass
@@ -24,53 +79,88 @@ class snake(object):
     def addCube(self):
         pass
 
+    def draw(self, surface):
+        for i,cube in enumerate(self.body):
+            #If it is the first cube of the snake, draw eyes (True), otherwise it is the body
+            if i == 0:
+                cube.draw(surface, True)
+            else:
+                cube.draw(surface)
+
 class cube(object):
-    def __init__(self, color):
-        pass
+    rows = 25
+    width = 700
+    def __init__(self, start, dirnx=1, dirny=0, color=(0,0,255)):
+        self.pos = start
+        self.dirnx = 0
+        self.dirny = 0
+        self.color = color
 
-    def move(self):
-        pass
+    def move(self, dirnx, dirny):
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
 
-    def draw(self):
-        pass
+    def draw(self, surface, eyes=False):
+        distance = self.width // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
 
-def drawGrid(w, rows, surface):
-    sizeBetween = w // rows
+        #Drawing cubs slightly smaller than the size of a 1x1 grid for a better appearance
+        pygame.draw.rect(surface, self.color, (i*distance+1,j*distance+1, distance-2, distance-2))
+
+        #If we are creating a snake head then draw an eye
+        if eyes:
+            center = distance // 2
+            radius = 3
+            #circleEye1 = (i*distance+center-radius, j*distance+8)
+            circleEye2 = (i*distance+distance-radius*2, j*distance+8)
+            #pygame.draw.circle(surface, (255,255,255), circleEye1, radius)
+            pygame.draw.circle(surface, (255,255,255), circleEye2, radius)
+
+def drawGrid(width, rows, surface):
+    #Calculating the distance between each horizontal/vertical line
+    sizeBetween = width // rows
     x = 0
     y = 0
     for i in range(rows):
         x = x + sizeBetween
         y = y + sizeBetween
 
-        #Drawing horizonal and vertical lines
-        pygame.draw.line(surface, (255,255,255), (x,0),(x,w))
-        pygame.draw.line(surface, (255,255,255), (0,y),(w,y))
+        #Drawing horizonal and vertical lines for game
+        pygame.draw.line(surface, (255,255,255), (x,0),(x,width))
+        pygame.draw.line(surface, (255,255,255), (0,y),(width,y))
 
 def redrawWindow(surface):
-    global rows, width
+    #Will update the window with any changes done within the game
+    global rows, width, s
     surface.fill((0,0,0))
+    s.draw(surface)
     drawGrid(width, rows, surface)
     pygame.display.update()
 
 def main():
-    global width, height, rows
+    global width, height, rows, s
     width = 700
     height = 700
-    rows = 20
+    rows = 25
+
+    #Create a window of given dimenstions
     window = pygame.display.set_mode((width, height))
 
+    #Creating a snake object
     s = snake((0,255,0), (10,10))
 
     flag = True
 
     clock = pygame.time.Clock()
 
+    #While loop keeping the game alive
     while flag:
         pygame.event.get()
         pygame.time.delay(50)
         clock.tick(10)
+        s.move()
         redrawWindow(window)
 
-
-    pass
 main()
